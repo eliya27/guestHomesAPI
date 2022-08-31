71c81317c3d6c401e11c0a82a00c1ae236540647
Eliya Gervas <eliyagervas27@gmail.com>
2022-08-31 21:28:40 +0300

    v3 expose-header added

--- app.js
+++ app.js
@@ -43,14 +43,14 @@ app.use(BodyParser.urlencoded({ extended: true }));
   next();
 });
 */
-/*app.use(function (req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});*/
+});
 
 app.use("/api/users", usersRoute);
 app.use("/api/users", users_authRoute);
