8cd5692652554faf4fe01f7497e0043f16a8a046
Eliya Gervas <eliyagervas27@gmail.com>
2022-08-31 20:28:27 +0300

    v1

--- app.js
+++ app.js
@@ -45,10 +45,10 @@ app.use(BodyParser.urlencoded({ extended: true }));
 */
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  /*res.header(
+  res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
-  );*/
+  );
   next();
 });
 
